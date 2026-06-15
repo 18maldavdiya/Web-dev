f1d2b63c1f86d1880907c99aaad290bb8ff95d5b
18maldavdiya <hanish2005b@gmail.com>
2026-06-15 14:08:34 +0530

    hospital

--- java Script/lectureClosue.js
+++ java Script/lectureClosue.js
@@ -1,3 +1,23 @@
-let fruits= ["apple","banana","Mango"];
+let array= ["apple","banana","Mango"];
 //length
-console.log(Array.length);
+console.log(array.length);
+//puch
+array.push("watermelon")
+console.log(array);
+
+//pop
+array.pop();
+console.log(array);
+
+//unshift
+array.unshift("papaya");
+console.log(array);
+ 
+
+//shift
+array.shift();
+console.log(array);
+
+//include
+
+console.log(array.includes("mango"));
