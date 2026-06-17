52b4c5e3d6fc8221087fc794f17588e224a1d740
18maldavdiya <hanish2005b@gmail.com>
2026-06-17 11:56:18 +0530

    product

---
+++ java Script/practise.js
@@ -0,0 +1,17 @@
+const names = ["gana", "isms", "hhfuih"];
+
+const q1 = names.map(function(name) {
+    return name.toUpperCase();
+});
+
+console.log(q1);
+
+//q2
+const product=[
+    {id:1,name:"Laptop"},
+    {id:2,name:"Table"},
+    {id:3,name:"Phone"}
+];
+ const p1 = product.map(function(Product) {
+    return product.name;
+});
