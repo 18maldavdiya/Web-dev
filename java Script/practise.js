881feb0957b12b7e5ff9b1e67d02d5eff7dbe3db
18maldavdiya <hanish2005b@gmail.com>
2026-06-18 12:37:02 +0530

    commit

--- java Script/practise.js
+++ java Script/practise.js
@@ -126,4 +126,20 @@
 // }, {});
 // console.log(q10);
 // console.log(q10arr);
+//
+
+const students = [{
+        name: "Utkarsh",
+        marks: [80, 90, 85]
+    },
+    {
+        name: "yukta",
+        marks: [95, 92, 98]
+    },
+    {
+        name: "Navneet",
+        marks: [60, 70, 65]
+    }
+];
+
 
