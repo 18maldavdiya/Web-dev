af8195c5aa4b03cd2775d93ec460fa7547d43bdd
18maldavdiya <hanish2005b@gmail.com>
2026-08-12 22:21:38 +0530

    instagram

--- Backend/Express/EJS/index.js
+++ Backend/Express/EJS/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 const port = 8080;
 
 app.set("view engine" , "ejs");
+app.set("views" , path.join(__dirname , "views"));
 
 app.get("/" , (req, res) => {
     res.render("home");
+})
+app.get("/hello" , (req, res) => {
+    res.send("Helloe");
+})
+
+app.get("/rolldise" , (req, res) => {
+    let dise =  Math.floor(Math.random() * 6) + 1 ;
+    res.render("rolldise" , { nums : dise});
+})
+
+app.get("/ig/:username" , (req , res) => {
+    const followers = ["jbin","Hanish","manish","rogit","shhit"];
+    const username = req.params.username;
+    console.log(username);
+    res.render("instagram.ejs" , {username , followers});
 })
 
 app.listen(port, () => {
