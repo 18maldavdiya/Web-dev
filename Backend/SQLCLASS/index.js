ee80aadd53f98a841a0ccb23209b512196aee342
18maldavdiya <hanish2005b@gmail.com>
2026-09-14 09:34:48 +0530

    commit

--- Backend/SQLCLASS/index.js
+++ Backend/SQLCLASS/index.js
@@ -1,12 +1,43 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database:'delta_app',
+    password: 'hanish'
+});
+let q = "INSERT INTO user (id,username,email,password) VALUES ?";
 
 let getRandomUser = () => {
-    return {
-        id: faker.string.uuid(),
-        username: faker.internet.username(),
-        email: faker.internet.email(),
-        password: faker.internet.password()
-    };
+    return [
+        faker.string.uuid(),
+        faker.internet.username(),
+        faker.internet.email(),
+        faker.internet.password()
+    ];
+};
+
+let data = [];
+
+for (let i = 1; i <= 100; i++) {
+    data.push(getRandomUser());
 }
 
-console.log(getRandomUser());
+console.log(data);
+
+try{
+connection.query(q,[data],(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+})
+}
+catch(err){
+    console.log(err);
+}
+
+connection.end();
+
+
+
+
